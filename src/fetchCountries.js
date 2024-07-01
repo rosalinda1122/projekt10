cc69501e894bf4cb34a3a56bba0522c3d5680992
Różowa <roserutkowska@gmail.com>
2024-07-01 20:00:24 +0200

    Create fetchCountries.js

---
+++ src/fetchCountries.js
@@ -0,0 +1,24 @@
+import Notiflix from "notiflix";
+
+const COUNTRIES_API_URL = 'https://restcountries.com/v3.1/name'
+
+const fetchCountries = async name => {
+    try {
+        const response = await fetch(
+            COUNTRIES_API_URL +
+            name +
+            'fields=name,capital,population,flags,languages'
+        );
+
+        if (response.status == 200) {
+            let json = await response.json();
+            return json;
+        }
+
+    } catch (error) {
+        throw new Error (response.status);
+    };
+
+};
+
+    export {fetchCountries};
