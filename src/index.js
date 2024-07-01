1a981329fb2cac0279f2c052163f26d0eebdd595
Różowa <roserutkowska@gmail.com>
2024-07-01 20:14:37 +0200

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import '.css/styles.css';
+import {fetchCountries } from '.fetchCountries';
+import debounce from 'lodash.debounce';
+import {Notify} from 'notiflix/build/notiflix-notify-aio';
+
+const inputCountry = document.querySelector ('input#search-box');
+const listCountry = document.querySelector ('.country-list');
+const infoCountry = document.querySelector ('country-info');
+
+const DEBOUNCE_DELAY = 300;
+
+function inputHandler(event) {
+    const searchInput = event.target.value.trim();
+
+    cleanCountry()
+    cleanListCountry()
+
+    fetchCountries(searchInput)
+    .then (data => {
+        if (data.length > 10) {
+            Notify.info('To many matches found. Please enter a more specific name');
+            return;
+
+        }
+        countryDataMarkup(data);
+
+            
+
+        
+    })
+    .catch(err => {
+        Notify.failure('Oops, there is no country with that name');
+    });
+}
