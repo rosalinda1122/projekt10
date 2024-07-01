ad585cc401f5373cef8e6228f3002c8936a4a11f
Różowa <roserutkowska@gmail.com>
2024-07-01 20:32:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 
-import {fetchCountries } from '.fetchCountries';
+import { fetchCountries } from 'fetchCountries';
 import debounce from 'lodash.debounce';
 import {Notify} from 'notiflix/build/notiflix-notify-aio';
 
