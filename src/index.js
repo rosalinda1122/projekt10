8f9e156a6a3eb929900997ac90fd4c6d8a8fcc20
Różowa <roserutkowska@gmail.com>
2024-07-02 20:26:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {Notify} from 'notiflix/build/notiflix-notify-aio';
 
 const inputCountry = document.querySelector ('input#search-box');
 const listCountry = document.querySelector ('.country-list');
-const infoCountry = document.querySelector ('country-info');
+const infoCountry = document.querySelector ('.country-info');
 
 const DEBOUNCE_DELAY = 300;
 
@@ -18,7 +18,7 @@ function inputHandler(event) {
     fetchCountries(searchInput)
     .then (data => {
         if (data.length > 10) {
-            Notify.info('To many matches found. Please enter a more specific name'
+            Notify.info('Too many matches found. Please enter a more specific name'
             );
             return;
 
@@ -34,7 +34,8 @@ function createListMarkup(data) {
     
     return data
     .map ( ( { name, flags}) =>
-    `<li class="country-list_item") data-country = '${name.common}'><img class="country-info");
+    `<li class="country-list_item" data-country = '${name.common}'><img class="country-info_item"</li>`
+)
 
     .join('');
 };
@@ -43,62 +44,54 @@ function createListMarkup(data) {
 
 
 function createDataMarkup(data) {
-const countryE1 = data [0];
-const {name, capital, population,flags, languages } = countryE1;
-return `
-<li class="country_item">
-    <div class="country_flag-name-container">
-        <img src="${flags.svg}" alt="${name.common}" height="30px"/></p>
-        <h1 class="country_title" >${name.official}</h2>
-        </div>
-        <p><b>Capital:</b> ${Capital}
-        <p><b>Population:</b> ${population}</p>
-        <p><b>Languages</b> ${Object.values(data[0].languages)}</p>
-        </li>
+    const countryE1 = data [0];
+    const { name, capital, population,flags, languages } = countryE1;
+        return `
+            <li class="country_item">
+                <div class="country_flag-name-container">
+                <img src="${flags.svg}" alt="${name.common}" height="30px"/></p>
+                <h1 class="country_title" >${name.official}</h2>
+                </div>
+                <p><b>Capital:</b> ${capital}
+                <p><b>Population:</b> ${population}</p>
+                <p><b>Languages:</b> ${Object.values(data[0].languages)}</p>
+            </li>
         `;
-        
-    function countryDataMarkup(data) {
+};
+
+
+function countryDataMarkup(data) {        
     if (data.length === 1) {
-    const dataMarkup = createDatamarkup(data);
-    info.Country.innerHTML = datamarkup;
+        const dataMarkup = createDataMarkup(data);
+        infoCountry.innerHTML = dataMarkup;
     } else {
-     const listMarkup = createListMarkup (data);
-    listCountry.innerHTML = listMarkup;
+        const listMarkup = createListMarkup (data);
+        listCountry.innerHTML = listMarkup;
     
-    const listCountryItem = document.querySelectorAll ('li');
-
-    listCountryItem.forEach(item => {
-        item.addEventListener('click',event => {
-            const clickedCountry = event.currentTarget.dataset.country;
+        const listCountryItem = document.querySelectorAll ('li');
+    
+        listCountryItem.forEach(item => {
+            item.addEventListener('click',  event => {
+                const clickedCountry = event.currentTarget.dataset.country;
+                const wantedCountry = data.filter (
+                    country => country.name.common === clickedCountry
+                );    
             
-            const wantedCountry = data.filter (
-            country => country.name.common === clickedCountry
-            );
-            
-            infoCountry.innerHTML = createDataMarkup(wantedCountry);
-            console.log('item clicked!', clickedCountry);
-            cleanListCountry();
-
-});
-
-});
+                infoCountry.innerHTML = createDataMarkup(wantedCountry);
+                console.log('item clicked!', clickedCountry);
+                cleanListCountry();
+            });        
+        });    
+    };    
 };
 
-};
 
 inputCountry.addEventListener('input', debounce(inputHandler, DEBOUNCE_DELAY));
 
-function cleanCountry() {
-
-infoCountry.innerHTML = '';
+function cleanCountry() {  
+    infoCountry.innerHTML = '';
 };
 
-funtion cleanListCountry() {
-listCountry.innerHTML = '';
+function cleanListCountry() {
+    listCountry.innerHTML = '';
 };
-    
-    
-    }}
-
-
-}
